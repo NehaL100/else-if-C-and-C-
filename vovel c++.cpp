930100770ade8fc769d06a1e48e70a1879e6373c
Muhammad Nehal <99079068+NehaL100@users.noreply.github.com>
2022-02-10 15:20:00 +0500

    Add files via upload

---
+++ vovel c++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	char ch,a;
+	cout<<"enter any character"<<endl;
+	cin>>ch;
+	if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+		cout<<"your character is a vovel"<<endl;
+		
+	}
+	else
+	{
+	cout<<"your character is not a vovel"<<endl;	
+	}
+	return 0;
+}
